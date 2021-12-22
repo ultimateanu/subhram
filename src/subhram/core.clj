85185b1355a48775cd3a8a234d0a96e90d0fa31c
Anu Bandi <ultimateanu@gmail.com>
2021-12-22 14:30:34 -0800

    got basic ui working

--- src/subhram/core.clj
+++ src/subhram/core.clj
@@ -1,7 +1,25 @@
 (ns subhram.core
-  (:gen-class))
+  (:gen-class)
+  (:require [cljfx.api :as fx])
+  (:import [javafx.application Platform]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn hello-world [& args]
+  {:fx/type :label
+   :text "Hello, World!"})
+
+(defn root [& args]
+  {:fx/type :stage
+   :showing true
+   :title "Subhram"
+   :scene {:fx/type :scene
+           :root {:fx/type :v-box
+                  :padding 250
+                  :spacing 40
+                  :children [{:fx/type hello-world}]}}})
+
+(def renderer
+  (fx/create-renderer))
+
+(defn -main [& args]
+  (Platform/setImplicitExit true)
+  (renderer {:fx/type root}))
