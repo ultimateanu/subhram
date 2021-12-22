a7dad04da6d21d3142be7d15c2aa62dcf1c1af3a
Anu Bandi <ultimateanu@gmail.com>
2021-12-22 14:07:40 -0800

    swing kinda works

--- src/subhram/core.clj
+++ src/subhram/core.clj
@@ -1,7 +1,22 @@
 (ns subhram.core
-  (:gen-class))
+  (:gen-class)
+  (:require [seesaw.core :as seesaw])
+  ;; (:use seesaw.core)
+  )
+
+(def button
+      (seesaw/button
+         :text "Click Me"
+         :listen [:action (fn [event](seesaw/alert "Click!" ))]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, Worldzzzz!")
+  (seesaw/invoke-later
+    (-> (seesaw/frame :title "Subhram",
+          ;;  :content "Hello, Seesaw",
+           :content button,
+           :on-close :exit)
+     seesaw/pack!
+     seesaw/show!)))
