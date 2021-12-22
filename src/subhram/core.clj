db0bea57b41fbbdd2aea603c0fd8e928c656c47d
Anu Bandi <ultimateanu@gmail.com>
2021-12-22 15:02:06 -0800

    added chooser

--- src/subhram/core.clj
+++ src/subhram/core.clj
@@ -1,13 +1,23 @@
 (ns subhram.core
   (:gen-class)
   (:require [seesaw.core :as seesaw])
+  (:require [seesaw.chooser])
   ;; (:use seesaw.core)
   )
 
 (def button
       (seesaw/button
          :text "Click Me"
-         :listen [:action (fn [event](seesaw/alert "Click!" ))]))
+        ;;  :listen [:action (fn [event](seesaw/alert "Click!" ))]
+         :listen [:action (fn [event]
+            (let [chosenFile (seesaw.chooser/choose-file)]
+              (println "Chose files:" (str chosenFile)))
+            ;; (seesaw.chooser/choose-file)
+            )]
+         ))
+
+(def lb (seesaw/scrollable (seesaw/listbox :model (-> 'seesaw.core ns-publics keys sort))))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
